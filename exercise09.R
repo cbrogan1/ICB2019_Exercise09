b281ae03c634023f2bbbac2b178cfb63a8758f9c
Cailey Brogan <caileybrogan@caileys-mbp.dhcp.nd.edu>
2019-11-25 15:15:59 -0500

    almost final draft

--- exercise09.R
+++ exercise09.R
@@ -6,28 +6,28 @@ library(cowplot)
 # number 1
 # data from https://www.cdc.gov/growthcharts/html_charts/wtage.htm about age vs weight in males 2-20 yrs old
 
-# read in file name from user
-filename <- readline("Enter file name (with .txt extension): ")
+# read in data
+dataFile <- read.table("age_weight.txt", header=T, sep="\t")
 
-# function that reads in file name and then 
-create_scatter <- function(filename) {
-  dataFile <- read.table(filename, header=T, sep="\t")
-  a = ggplot(data = dataFile, aes(x = age, y = weight)) + 
-      geom_point(color = "blue", shape = 1) + 
-      theme_bw() + 
-      xlab("Age (in months)") + 
-      ylab("50th percentile weight (in kg)") +
-      stat_smooth(method="lm") + theme_classic()
-  a
-}
+# create plot
+a = ggplot(data = dataFile, aes(x = age, y = weight)) + 
+    geom_point(color = "blue", shape = 1) + 
+    theme_bw() + 
+    xlab("Age (in months)") + 
+    ylab("50th percentile weight (in kg)") +
+    stat_smooth(method="lm") + theme_classic()
 
+# display plot
+a
 
 #--------------------------------------------------------------------------------------------------------------#
+
 # number 2
 data<-read.table("data.txt", header=T, sep=",")
 
 # bar graph of the average observations per region
 b <- ggplot(data, aes(x = region, y = observations)) +
+    xlab("Region") + ylab("Observations") +
     stat_summary(geom="bar", fun.y="mean")
 
 # scatter plot (using jitter) of all observations
@@ -37,4 +37,15 @@ c <- ggplot(data, aes(x= region, y = observations)) + geom_jitter(aes(color = as
 
 # plot both
 plot_grid(b, c)
+
+# Why the plots are different:
+# The difference in the bar and scatter plots show that the standard deviations of the regions are different.
+# The north region has a small standard deviation.
+# The east and west have large standard deviations.
+# The south region has two subsets of data that each have small deviations with in the data subsets but the data set as a whole has a greater standard deviation than the north.
+
+
+
+
+
 
